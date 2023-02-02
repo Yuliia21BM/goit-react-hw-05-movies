6961f5a6d495e854c222b09a74cd09ca86a24ca5
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-02-02 23:33:00 +0200

    Add folder with pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
 
-import { Header } from './Header/Header';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+import { HomeWrap } from '../pages/HomeWrap/HomeWrap';
+const MovieWrap = lazy(() => import('../pages/MovieWrap/MovieWrap'));
+const MovieInfo = lazy(() => import('../pages/MovieInfo/MovieInfo'));
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Header />}></Route>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomeWrap />} />
+        <Route path="/movieWrap" element={<MovieWrap />}></Route>
+        <Route path="/movieInfo" element={<MovieInfo />}></Route>
+      </Route>
     </Routes>
   );
 };
---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,14 @@
+// import { Suspense } from 'react';
+// import { Outlet } from 'react-router-dom';
+import { StyledHeader } from './SharedLayout.styled';
+
+export const SharedLayout = () => {
+  return (
+    <StyledHeader>
+      {/* <nav>
+        <Link to="/">Home</Link>
+        <Link to="/movies">Movies</Link>
+      </nav>*/}
+    </StyledHeader>
+  );
+};
