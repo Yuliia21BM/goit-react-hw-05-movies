a91231ff2919da64e22e439e660cd5a1d6afd833
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-02-04 22:32:35 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,36 @@
-// import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 // import { lazy } from 'react';
 
 import { SharedLayout } from './SharedLayout/SharedLayout';
+
+import { MovieWrap } from '../pages/MovieWrap/MovieWrap';
 import { MovieInfo } from '../pages/MovieInfo/MovieInfo';
+import { HomeWrap } from '../pages/HomeWrap/HomeWrap';
+import { MovieReview } from '../components/MovieReviews/MovieReviews';
+import { MovieCast } from '../components/MovieCast/MovieCast';
 // const MovieWrap = lazy(() => import('../pages/MovieWrap/MovieWrap'));
 // const MovieInfo = lazy(() => import('../pages/MovieInfo/MovieInfo'));
+// const HomeWrap = lazy(() => import('../pages/HomeWrap/HomeWrap'));
+// const MovieReview = lazy(() =>
+//   import('../components/MovieReviews/MovieReviews')
+// );
+// const MovieCast = lazy(() => import('../components/MovieCast/MovieCast'));
 
 export const App = () => {
   return (
-    <>
-      <SharedLayout />
-      <MovieInfo></MovieInfo>
-    </>
-    // <Routes>
-    //   <Route path="/" element={<SharedLayout />}>
-    //     <Route index element={<HomeWrap />} />
-    //     <Route path="/movieWrap" element={<MovieWrap />}></Route>
-    //     <Route path="/movieInfo" element={<MovieInfo />}></Route>
-    //   </Route>
-    // </Routes>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomeWrap />} />
+        <Route path="/:id" element={<MovieInfo />}>
+          <Route path="movieReview" element={<MovieReview />} />
+          <Route path="movieCast" element={<MovieCast />} />
+        </Route>
+        <Route path="movieWrap" element={<MovieWrap />} />
+        <Route path="movieWrap/:id" element={<MovieInfo />}>
+          <Route path="movieReview" element={<MovieReview />} />
+          <Route path="movieCast" element={<MovieCast />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 };
